9cc18b33101aaa55bf59fbe33309e214de8775c4
Casey Labrack <clabrack@gmail.com>
2023-12-01 02:16:03 -0500

    problem with overlap?

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -2,31 +2,58 @@ use std::fs;
 use regex::Regex;
 
 fn main() {
-    let data = fs::read_to_string("data/input.txt").expect("couldn't read puzzle input, dummy");
+    let data = fs::read_to_string("data/example2.txt").expect("couldn't read puzzle input, dummy");
     let data : Vec<&str> = data.lines().collect();
     
-    let is_digit = Regex::new(r"[0-9]").unwrap();
+    let digitlike = Regex::new(r"[0-9]|one|two|three|four|five|six|seven|eight|nine").unwrap();
     let mut sum = 0;
 
     for line in data {
 
-        let digit_chars: Vec<&str> = is_digit
+        let digitlikes: Vec<&str> = digitlike
             .find_iter(line)
             .map(|m| m.as_str())
             .collect();
 
-        let mut num = digit_chars.get(0).unwrap().to_string();
-        num.push_str(digit_chars.last().unwrap());
-        sum += num.parse::<i32>().unwrap(); 
-        println!("{}", num);
+        for d in &digitlikes {
+            print!("{}",d);
+        }
+        println!("");
+
+
+        let mut first = digitlikes[0];
+        if first.len() > 1 {
+            first = digitWordToDigit(first);
+        }
+        let mut last = digitlikes[digitlikes.len()-1];
+        if last.len() > 1 {
+            last = digitWordToDigit(last);
+        }
+
+        // println!(" {}{}", first,last);
+        
+        let mut num = first.to_string();
+        num.push_str(last);
+        
+        sum += num.parse::<i32>().unwrap();
     }
+
 
     println!("sum {}", sum);
 
 }
 
-fn firstAndLastDigitsHard (s:&str) {
-    let as_chars = s.chars().collect::<Vec<char>>();
-
-    // for char in 
+fn digitWordToDigit (s: &str) -> &str {
+    match s {
+        "one" => "1",
+        "two" => "2",
+        "three" => "3",
+        "four" => "4",
+        "five" => "5",
+        "six" => "6",
+        "seven" => "7",
+        "eight" => "8",
+        "nine" => "9",
+        _ => unreachable!()
+    }
 }
